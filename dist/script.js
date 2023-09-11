9b76dfcdaf192e9558056c7dc6372c8775dc529e
Francisvan Oliveira <francisvan.oliveira@outlook.com>
2023-09-10 22:15:32 -0400

    🆙 Atualização
    Realizado atualização no JS

--- dist/script.js
+++ dist/script.js
@@ -6,10 +6,11 @@ const resultadoElemento = document.getElementById('resultado');
 // evaluate string '10 + 5 - 2' to number 13
 function evaluate(expression) {
     try {
-        if (expression.match(/[a-zA-Z&#$<>{}]/g))
+        const expressionWithDots = expression.replace(/,/g, '.');
+        if (expressionWithDots.match(/[a-zA-Z&#$<>{}]/g))
             throw new Error();
         // cuidado com Function
-        return new Function(`return (${expression})`)();
+        return new Function(`return (${expressionWithDots})`)();
     }
     catch (e) {
         return null;
@@ -27,22 +28,25 @@ function isNumber(value) {
 function round(value) {
     return Math.round(value * 1000) / 1000;
 }
+function formatDecimal(value) {
+    return value.toString().replace('.', ',');
+}
 function calcular() {
     // save calculadora value to localStorage
     localStorage.setItem('calculadora', calculadoraElemento.value);
     // separate lines from calculadoraElemento in array
     const linhas = calculadoraElemento.value.split(/\r?\n/).map(evaluate);
     resultadoElemento.innerHTML = `<div>${linhas
-        .map((l) => `<div>${isNumber(l) ? round(l) : '---'}</div>`)
+        .map((l) => `<div>${isNumber(l) ? formatDecimal(round(l)) : '---'}</div>`)
         .join('')}</div>`;
     // calculate total from linhas
     const total = round(linhas.filter(isNumber).reduce((acc, cur) => acc + cur, 0));
     // add total to resultadoElemento
-    resultadoElemento.innerHTML += `<div id="total">${total}</div>`;
+    resultadoElemento.innerHTML += `<div id="total">${formatDecimal(total)}</div>`;
     // save total to clipboard on click
     const totalElemento = document.getElementById('total');
     totalElemento.addEventListener('click', () => {
-        navigator.clipboard.writeText(total.toString());
+        navigator.clipboard.writeText(formatDecimal(total));
     });
 }
 // load calculadora value from localStorage
@@ -50,4 +54,3 @@ calculadoraElemento.value = localStorage.getItem('calculadora') || '';
 // add event listener to calculadoraElemento
 calculadoraElemento.addEventListener('input', calcular);
 calcular();
-//# sourceMappingURL=script.js.map
